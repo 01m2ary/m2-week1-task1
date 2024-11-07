b729a21be48c32b4599da1e3f0e62335097111c4
01m2ary <021mary0@gmail.com>
2024-11-07 20:39:49 +0300

    content01

--- src/components/content/content.jsx
+++ src/components/content/content.jsx
@@ -1,20 +1,24 @@
 import "./content.css";
-const content =()=>{
+import ContentImage from './Content.png';
+
+const Content = () => {
   return (
     <div className="content">
-      <img src="Content.png" alt="Content" />
-      <h1>Welcome to Your <br /> Freelancing Hub!</h1>
-      <p>
-        We’re thrilled to have you here! Whether you're a client <br /> 
-        searching for skilled freelancers to bring your projects to life or<br /> 
-        a freelancer eager to find exciting opportunities, this is the<br /> 
-        place where connections are made, and ideas come to reality.<br /> 
-        Let's start building something amazing together!
-      </p>
-
+      <div className="image-container">
+        <img src={ContentImage} alt="Content" /> 
+        <div className="text-overlay">
+          <h1>Welcome to Your <br /> Freelancing Hub!</h1>
+          <p>
+            We’re thrilled to have you here! Whether you're a client <br /> 
+            searching for skilled freelancers to bring your projects to life or<br /> 
+            a freelancer eager to find exciting opportunities, this is the<br /> 
+            place where connections are made, and ideas come to reality.<br /> 
+            Let's start building something amazing together!
+          </p>
+        </div>
+      </div>
     </div>
-
   );
-}
+};
 
-export default content;
+export default Content;
