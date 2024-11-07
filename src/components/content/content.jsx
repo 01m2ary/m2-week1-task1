bc70a29950ea0c4df475640016bc0fd05d13e4f9
01m2ary <021mary0@gmail.com>
2024-11-07 19:51:01 +0300

    "content"

---
+++ src/components/content/content.jsx
@@ -0,0 +1,20 @@
+import "./content.css";
+const content =()=>{
+  return (
+    <div className="content">
+      <img src="Content.png" alt="Content" />
+      <h1>Welcome to Your <br /> Freelancing Hub!</h1>
+      <p>
+        We’re thrilled to have you here! Whether you're a client <br /> 
+        searching for skilled freelancers to bring your projects to life or<br /> 
+        a freelancer eager to find exciting opportunities, this is the<br /> 
+        place where connections are made, and ideas come to reality.<br /> 
+        Let's start building something amazing together!
+      </p>
+
+    </div>
+
+  );
+}
+
+export default content;
